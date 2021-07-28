ca96f1c3d0afe562c6f589ed70d9982950aca890
Shehzad Shaikh <cyberrspiritt@Shehzads-MacBook.local>
2021-07-28 15:57:00 +0530

    updated script to fix an issue with false IPv6 identification at ipconfig.co when connected via mobile hotspot

--- updateIp.sh
+++ updateIp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to pull my current public IP address
 # and add a rule to my EC2 security group allowing me SSH access
-curl v4.ifconfig.co > ip.txt
+curl https://checkip.amazonaws.com > ip.txt
 awk '{ print $0 "/32" }' < ip.txt > ipnew.txt
 export stuff=$(cat ipnew.txt)
 aws ec2 revoke-security-group-ingress --group-name User_Ip --ip-permissions "`aws ec2 describe-security-groups --output json --group-name User_Ip --query "SecurityGroups[0].IpPermissions" --profile propyoda`" --profile propyoda
